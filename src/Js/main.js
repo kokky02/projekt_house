b4c6bc465d3b5c69e7a3ba0e09c7fe663ce92bb0
Daniel_Kokay <daniel.kokay@gmail.com>
2022-05-22 17:45:21 +0200

    napojení obrázků a slideru

--- src/Js/main.js
+++ src/Js/main.js
@@ -25,4 +25,25 @@ $(document).ready(function(){
         modal.removeClass('modal_active')
         
     }); 
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                }
+              }
+        ]
+    });
+
 });
